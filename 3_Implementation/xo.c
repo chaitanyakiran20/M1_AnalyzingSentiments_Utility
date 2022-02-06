c8c2047de1ff87ae5931d8c390dfb9cf057e4d9c
chaitanyakiran20 <chaitanyakiran20@gmail.com>
2022-02-06 12:24:35 +0530

    update 6/02/2022

--- 3_Implementation/xo.c
+++ 3_Implementation/xo.c
@@ -5,7 +5,21 @@
  * 
  * @return int 
  */
+#include <stdio.h>
+void info_display();
+
 int main()
 {
+    void info_display();
+
     return 0;
 } 
+void info_display()
+{
+    printf("Xs0s\n");
+    printf("Welcome to the World of 2Dimensional Games, Introducing X and O\n\n");
+    printf("info_display:-\n");
+    printf("\n1:Each player will be Allowed to place their own Character, either X or O in the desired position");
+    printf("\n2:Player who gets a combination of 3 same characters of his own either diagonal or horizontally or \n  vertically will be declared as the winner");
+    printf("\n\nEnjoy the Game!\n");
+}
